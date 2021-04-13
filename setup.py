625bd1a61c11cb81c452a06278a16508032d141e
tamari-oz <72387668+tamari-oz@users.noreply.github.com>
2021-04-13 21:06:36 +0300

    PSD-64 (#13)
    * 1. Bug fix on SDK get_licenses
    2. Added accidental Secret commit discovery
    3. 1st commit on SPDX BOM feature
    
    * 1. Bug fix on SDK get_licenses
    2. Added accidental Secret commit discovery
    3. 1st commit on SPDX BOM feature
    
    * WS SBOM commit
    
    * WS SBOM commit
    
    * WS SBOM commit
    
    * Added copyright and aux utility to convert lists to dicts in SDK
    
    * 1. Break CI to master an non master push
    2. Bump SDK to 1.2
    
    * 1. Break CI to master an non master push
    2. Bump SDK to 1.2
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Fixing CI
    
    * Delete params.config
    
    * Delete projects_cleanup.py
    
    * Delete requirements.txt
    
    * Delete sbom_report.py
    
    * Delete __init__.py
    
    * Delete sbom_extra.json
    
    * Added org details to scope
    
    * Bug fix in case no org details
    
    * Revert "Delete sbom_report.py"
    
    This reverts commit 2dcd2431
    
    * Revert "Delete projects_cleanup.py"
    
    This reverts commit 3315d9e1
    
    * Revert "Delete params.config"
    
    This reverts commit ec216d92
    
    * Revert "Delete __init__.py"
    
    This reverts commit b777fab2
    
    * Added SBOM as example
    
    * Changed to 0.1.1
    
    * Update web.py
    
    * Update ci-master.yml

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="ws_sdk",
-    version="0.1.0-1",
+    version="0.1.1",
     author="WhiteSource Professional Services",
     author_email="ps@whitesourcesoftware.com",
     description="WS Python SDK",
--- ws_sdk/ws_utilities.py
+++ ws_sdk/ws_utilities.py
@@ -1,2 +1,37 @@
+import logging
 def is_token(token: str) -> bool:
     return True if len(token) == 64 and token.isalnum() else False
+
+def convert_dict_list_to_dict(lst: list,
+                              key_desc: str or tuple) -> dict:
+    """
+    Function to convert list of dictionaries into dictionary of dictionaries according to specified key
+    :param lst: List of dictionaries
+    :param key_desc: the key or keys (as tuple) description of the returned dictionary
+    :return: dict with key according to key description and the dictionary value
+    """
+    def create_key(key_desc: str or tuple,
+                   dct: dict) -> str or tuple:
+        ret = None
+        if isinstance(key_desc, str):
+            return dct[key_desc]
+        elif isinstance(key_desc, tuple):
+            ret = []
+            for x in key_desc:
+                try:
+                    ret.append(dct[x])
+                except KeyError:
+                    logging.error(f"Key: {key_desc} was not found")
+                    return None
+            logging.debug(f"Key is tuple: {ret}")
+            return tuple(ret)
+        else:
+            logging.error(f"Unsupported key_desc: {type(key_desc)}")
+            return None
+
+    ret = {}
+    for i in lst:
+        curr_key = create_key(key_desc, i)
+        ret[curr_key] = i
+
+    return ret
