98de1a7ec55ab568ea6eb178a6c3d1eee1ea1b39
NatDal <63256867+NatalyaDalid@users.noreply.github.com>
2021-05-03 14:20:18 +0300

    Update setup.py (#19)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name="ws_sdk",
-    version="0.1.3",
+    version="0.1.4",
     author="WhiteSource Professional Services",
     author_email="ps@whitesourcesoftware.com",
     description="WS Python SDK",
