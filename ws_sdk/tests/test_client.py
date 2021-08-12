e2e6f8e84c87d359da254ceedb27831209dbf524
OzTamari <oz.tamari@whitesourcesoftware.com>
2021-08-12 19:33:50 +0300

    Fixed unittests

--- ws_sdk/tests/test_client.py
+++ ws_sdk/tests/test_client.py
@@ -14,10 +14,13 @@ class TestWSClient(TestCase):
 
 
 class TestWS(TestCase):
-    def setUp(self):
+    @patch('ws_sdk.ws_utilities.convert_ua_conf_f_to_vars')
+    def setUp(self, mock_convert_ua_conf_f_to_vars):
         logging.basicConfig(level=logging.DEBUG)
         pathlib.Path("ws_constants.DEFAULT_UA_PATH").mkdir(parents=True, exist_ok=True)
-
+        mock_convert_ua_conf_f_to_vars.return_value.apiKey = None
+        mock_convert_ua_conf_f_to_vars.return_value.userKey = None
+        mock_convert_ua_conf_f_to_vars.return_value.wss_url = None
         self.client = WSClient(user_key=os.environ['WS_USER_KEY'],
                                token=os.environ['WS_ORG_TOKEN'])
 
